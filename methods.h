7fa8aea9e203ac3ab3ef5f63c53c9634017f14f7
shivomPatel <37147271+shivomPatel@users.noreply.github.com>
2019-07-08 19:00:46 -0400

    Create methods.h

---
+++ methods.h
@@ -0,0 +1,10 @@
+// Prototypes
+float calculate_differential_nine(float, float, float);
+float calculate_differential_eighteen(float, float, float);
+void record_differential_nine(char* , float);
+void record_differential_eighteen(char*, float);
+float fetch_nine_handicap_index(char*);
+float fetch_eighteen_handicap_index(char*);
+float* array_sort(float*, int);
+void swap(float*, float*);
+void print_arr(float*, int);
